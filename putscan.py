93ea096311a76a61bd7fe01040cd29a6d5d25cc6
historypeats <iam.historypeats@gmail.com>
2013-09-11 16:09:46 -0400

    Initial Commit
    Initial Commit

---
+++ putscan.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+__author__ = 'historypeats'
+import requests
+import sys
+
+data = 'About that fox...'
+testFile = 'someTestFile.txt'
+proxies = {}
+
+
+def main():
+    """
+
+    Main
+    """
+
+    if len(sys.argv) < 2:
+        sys.exit("Need urls file.")
+
+    try:
+        urlFile = open(sys.argv[1], 'r')
+    except IOError:
+        sys.exit('Unable to open file: ' + sys.argv[1])
+
+    for url in urlFile:
+        puturl = url.strip() + testFile
+
+        try:
+            response = requests.put(puturl, data=data)
+            if response.status_code == 200:
+                print "[VULNERABLE!] - " + puturl
+            else:
+                print "[" + str(response.status_code) + "] - " + puturl
+        except requests.ConnectionError:
+            print 'Connection failed: ' + url
+        except requests.Timeout:
+            print 'Connection timed out: ' + url
+        except requests.HTTPError:
+            print 'Invalid HTTP Response: ' + url
+        except Exception:
+            print 'Unknown error: ' + url
+
+if __name__ == '__main__':
+    main()
+
